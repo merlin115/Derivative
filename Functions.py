43f81c358a7c7008c685ff7e26954939449dcad0
Daniel Zhou <31249310+Adreambottle@users.noreply.github.com>
2020-12-03 01:11:09 +0800

    修改了 get_r

--- Functions.py
+++ Functions.py
@@ -3,6 +3,9 @@ import numpy as np
 import tushare as ts
 import datetime
 import math
+from scipy.special import comb
+
+from math import log, sqrt, exp
 
 class download_data():
 
@@ -28,22 +31,32 @@ class download_data():
         df.sort_index(inplace=True)
         return df
 
+    def get_dividend(self):
+        df = self.pro.daily(ts_code=self.SC,
+                            start_date = self.sta_date,
+                            end_date=self.end_date)
+        df.index = pd.to_datetime(df["trade_date"], format="%Y%m%d")
+        df.sort_index(inplace=True)
+
+
     def get_r(self, type = "shibor"):
         if type == "libor":
-            df = self.pro.libor(curr_type='RMB',
-                                start_date = self.sta_date,
-                                end_date=self.end_date)
-        if type == "shibor":
-            df = self.pro.shibor(curr_type='USD',
+            df = self.pro.libor(curr_type='USD',
                                 start_date=self.sta_date,
                                 end_date=self.end_date)
+        if type == "shibor":
+            df = self.pro.shibor(start_date=self.sta_date,
+                                 end_date=self.end_date)
         if type == "hibor":
-            df = self.pro.hibor(curr_type='USD',
+            df = self.pro.hibor(curr_type='RMB',
                                 start_date=self.sta_date,
                                 end_date=self.end_date)
 
         df.index = pd.to_datetime(df["date"], format="%Y%m%d")
         df.sort_index(inplace=True)
+        return df["1y"]
+
+
 
 
 
@@ -85,6 +98,176 @@ def get_volatility(df, method = "percentage"):
 # get_volatility(close)
 
 
+def Price_by_BT(r, sigma, delta, K, S0, h, T):
+    pass
+    # r = 0.035
+    # delta = 0.02
+    # sigma = 1.2
+    # h = 1/365
+    # K = 120
+    # S0 = 100
+    #
+    # n = int(T/h)
+    # u = exp((r-delta) * h + sigma*sqrt(h))
+    # d = exp((r-delta) * h - sigma*sqrt(h))
+    #
+    # c_u = max(0, u*S0 - K)
+    # c_d = max(0, d*S0 - K)
+    # p = ((1 + r - sigma) - d)/(u - d)
+    #
+    # comb_list = []
+    # option_value_list = []
+    # for i in range(0, n+1):
+    #     comb_value = int(comb(n, i))
+    #     u_coef = i
+    #     d_coef = n + 1 - i
+    #     option_value_pred = K - S0 * u**u_coef * d**d_coef
+    #     option_value_adju = max()
+    #     comb_list.append(comb_value)
+    # max(0, K - S0 * u )
+    # for i in range((0, n+1):
+    #     c = (p * c_u + (1 - p) * c_d)/(1 + r - delta)
+    # return c
+
+def get_option_by_s(ST, X, alpha = 0.3):
+    if alpha * ST >= ST - X >= 0:
+        option = ST - X
+    if ST - X > alpha * ST:
+        option = alpha * ST
+    if ST - X < 0:
+        option = 0
+    return option
+
+
+
+
+
+def Price_by_BT_2(r, sigma, delta, X, S0, h, alpha = 0.3):
+
+    u = math.exp((r-delta) * h + sigma*math.sqrt(h))
+    d = math.exp((r-delta) * h - sigma*math.sqrt(h))
+    p = (exp((r-delta) * h) - d)/(u-d)
+    q = 1 - p
+    uS = u * S0
+    dS = d * S0
+    uOp = get_option_by_s(uS, X, alpha)
+    dOp = get_option_by_s(dS, X, alpha)
+    sanjiao = exp(-delta*h) * (uOp - dOp)/(S0*(u-d))
+    B = exp(-r*h) * (u * dOp - d * uOp)/(u-d)
+    output_dic = {"Call_Up":uOp,
+                  "Call_down":dOp,
+                  "Stock_Up":uS,
+                  "Stock_Down":dS,
+                  "Up_factor":u,
+                  "Down_factor":d,
+                  "p_star":p,
+                  "delta":sanjiao,
+                  "B":B}
+    return output_dic
+
+r = 0.035
+delta = 0.02
+sigma = 1.2
+h = 1/365
+X = 120
+S0 = 100
+alpha = 0.3
+
+Price_by_BT_2(r, sigma, delta, X, S0, h, alpha)
+get_option_by_s(100, 100, 0.3)
+
+
+
+class Option:
+    def __init__(self, r, sigma, delta, X, S0, h, alpha):
+        self.r = r
+        self.sigma = sigma
+        self.delta = delta
+        self.X = X
+        self.S0 = S0
+        self.h = h
+        self.alpha = alpha
+
+    def One_step_BT(self):
+        pass
+
+
+
+def Build_Tree_E(n, sigma, delta, X, S0_P, h, alpha):
+    option_matrix = []
+    # item = {"Stock":100, "Option":0}
+    option = [{"Stock":100, "Call":0}]
+    option_matrix.append(option)
+    for i in range(1, n):
+        option_list = []
+        for j in range(0, i):
+            S0_P = option_matrix[i - 1][j]["Stock"]
+            if j != i-1:
+                SU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Stock_Up"]
+                CU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Call_Up"]
+                option = {"Stock":SU, "Call":CU}
+                option_list.append(option)
+            if j == i-1:
+                SU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Stock_Up"]
+                CU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Call_Up"]
+                option = {"Stock":SU, "Call":CU}
+                option_list.append(option)
+
+                SD = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Stock_Down"]
+                CD = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Call_down"]
+                option = {"Stock":SD, "Call":CD}
+                option_list.append(option)
+
+        option_matrix.append(option_list)
+    return option_matrix.append(option_list)
+
+
+def Build_Tree_A(n, sigma, delta, X, S0_P, h, alpha):
+    option_matrix = []
+    # item = {"Stock":100, "Option":0}
+    option = [{"Stock":100, "Call":0}]
+    option_matrix.append(option)
+    for i in range(1, n):
+        option_list = []
+        for j in range(0, i):
+            S0_P = option_matrix[i - 1][j]["Stock"]
+            if j != i-1:
+                SU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Stock_Up"]
+                CU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Call_Up"]
+                C_cdt = X - SU
+                if C_cdt > CU:
+                    option = {"Stock":SU, "Call":C_cdt}
+                else:
+                    option = {"Stock":SU, "Call":CU}
+                option_list.append(option)
+
+            if j == i-1:
+                SU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Stock_Up"]
+                CU = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Call_Up"]
+                C_cdt = X - SU
+                if C_cdt > CU:
+                    option = {"Stock":SU, "Call":C_cdt}
+                else:
+                    option = {"Stock":SU, "Call":CU}
+                option_list.append(option)
+
+
+                SD = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Stock_Down"]
+                CD = Price_by_BT_2(r, sigma, delta, X, S0_P, h, alpha)["Call_down"]
+                C_cdt = X - SD
+                if C_cdt > CD:
+                    option = {"Stock":SD, "Call":C_cdt}
+                else:
+                    option = {"Stock":SD, "Call":CD}
+                option_list.append(option)
+
+        option_matrix.append(option_list)
+    return option_matrix.append(option_list)
+
+
+
+Build_Tree_A(n, sigma, delta, X, S0_P, h, alpha)
+
 
 def main():
     print("The stock code is: ")
@@ -98,7 +281,7 @@ def main():
     data = project.get_stock_data()
     df_r = project.get_r()
 
-    pro.shibor(curr_type="RMB", start_date=sta_date, end_date=end_date)
+    # pro.shibor(curr_type="RMB", start_date=sta_date, end_date=end_date)
 
 
     d_sta_date = datetime.datetime.strptime(sta_date, "%Y%m%d")
@@ -106,4 +289,12 @@ def main():
 
 
 
+# V = exp(-r*(T-t)) * max((ST-X), 0)
+# St = S0 * e(mu-sigma-sigma**2/2 + sigma*sqrt(t))
+
+K = X/(1 - alpha)
+d1 = (log(ST/X) + r - delta + 0.5*sigma**2)/(sigma*sqrt(T-t))
+d2 = d1 - sigma*sqrt(T-t)
+d3 = (log(ST/K) + r - delta + 0.5*sigma**2)/(sigma*sqrt(T-t))
+d4 = d3 - sigma*sqrt(T-t)
 
